8cbea8175d5da2d4b8e2d3b784b9275387b380a4
Chris Taylor <forgetso86@gmail.com>
2024-03-05 10:48:46 +0000

    src assets

--- procaptcha-wordpress-integration/src/js/vite.cjs.config.ts
+++
@@ -1,7 +0,0 @@
-import {ViteCommonJSConfig} from '@prosopo/config'
-import path from 'path'
-
-export default function () {
-    //TODO this will not work out of the box. Please get the ViteCommonJSConfig from @prosopo/config and modify to suit your needs
-    return ViteCommonJSConfig('procaptcha-wordpress', path.resolve('./tsconfig.cjs.json'))
-}
